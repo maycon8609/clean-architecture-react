562a3495b3dd1cfe90f1ed0836bf94da23d0f0d5
maycon silva <maycon8609@gmail.com>
2023-05-27 17:39:01 -0300

    refactor: fixed lint error

--- src/data/usecases/authentication/remote-authentication.ts
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -1,13 +1,14 @@
-import type { HttpPostClient } from '../../protocols/http/http-post-client'
-import { type AuthenticationParams } from '../../../domain/usecases/authentication'
+import { HttpPostClient } from '../../protocols/http/http-post-client'
+import { AuthenticationParams } from '../../../domain/usecases/authentication'
 
 export class RemoteAuthentication {
-  constructor (
+  // eslint-disable-next-line no-useless-constructor
+  constructor(
     private readonly url: string,
     private readonly httpPostClient: HttpPostClient
   ) {}
 
-  async auth (params: AuthenticationParams): Promise<void> {
+  async auth(params: AuthenticationParams): Promise<void> {
     await this.httpPostClient.post({
       url: this.url,
       body: params
