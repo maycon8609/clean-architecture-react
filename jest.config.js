9903be424a16d7f8a5603a042cf2b72f8aacf655
maycon silva <maycon8609@gmail.com>
2023-06-24 21:56:51 -0300

    chore: add relative path

--- jest.config.js
+++ jest.config.js
@@ -11,6 +11,7 @@ module.exports = {
     '@domain/(.*)': ['<rootDir>/src/domain/$1'],
     '@infra/(.*)': ['<rootDir>/src/infra/$1'],
     '@main/(.*)': ['<rootDir>/src/main/$1'],
+    '@presentation/(.*)': ['<rootDir>/src/presentation/$1'],
     '\\.scss$': 'identity-obj-proxy'
   }
 }
