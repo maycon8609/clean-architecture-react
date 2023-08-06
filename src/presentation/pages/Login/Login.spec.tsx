6f574c5d1af3d8b73aa5f40a66c847c59cd90fa0
maycon silva <maycon8609@gmail.com>
2023-08-06 15:50:05 -0300

    refactor: removed import to react

--- src/presentation/pages/Login/Login.spec.tsx
+++ src/presentation/pages/Login/Login.spec.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 import { render, screen } from '@testing-library/react'
 import type { RenderOptions } from '@testing-library/react'
 
--- src/presentation/routes.tsx
+++ src/presentation/routes.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 import { createBrowserRouter } from 'react-router-dom'
 
 import { Login } from '@presentation/pages'
